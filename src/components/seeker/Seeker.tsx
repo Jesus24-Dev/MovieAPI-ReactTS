8f5aa06f22dcd3e55f3d998301e462273d743e1f
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-17 13:18:05 -0400

    Change ResultList to MovieCard component name

--- src/components/seeker/Seeker.tsx
+++ src/components/seeker/Seeker.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import InputSeeker from "./InputSeeker";
-import ResultList from "./ResultList";
+import MovieCard from "./MovieCard";
 
 interface Movie {
   Title: string;
@@ -103,7 +103,7 @@ const Seeker: React.FC<SeekerProps> = ({ onMovieClick }) => {
         <>
           <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {movies.Search.map((movie) => (
-              <ResultList
+              <MovieCard
                 key={movie.imdbID}
                 Title={movie.Title}
                 Year={movie.Year}
