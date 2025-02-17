eeaee65efbb77df605e2137112a5807a8a62e2ec
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-17 13:27:14 -0400

    use useQuery hook for fetch movie search

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import Seeker from "./components/seeker/Seeker";
 import MovieData from "./components/MovieData";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar el modal
-  const [selectedMovieId, setSelectedMovieId] = useState<string | null>(null); // Estado para almacenar el imdbID
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovieId, setSelectedMovieId] = useState<string | null>(null);
 
-  // Función para abrir el modal con el imdbID de la película seleccionada
   const handleOpenModal = (imdbID: string) => {
     setSelectedMovieId(imdbID);
     setIsModalOpen(true);
   };
 
-  // Función para cerrar el modal
   const handleCloseModal = () => {
     setIsModalOpen(false);
     setSelectedMovieId(null);
@@ -20,17 +21,16 @@ function App() {
 
   return (
     <>
-      {/* Seeker con la función para abrir el modal */}
-      <Seeker onMovieClick={handleOpenModal} />
-
-      {/* Modal con MovieData */}
-      {isModalOpen && selectedMovieId && (
-        <div className="fixed inset-0 bg-black/75 flex justify-center items-center p-4">
-          <div className="bg-white rounded-lg shadow-lg max-w-2xl w-full overflow-hidden">
-            <MovieData imdbID={selectedMovieId} onClose={handleCloseModal} />
+      <QueryClientProvider client={queryClient}>
+        <Seeker onMovieClick={handleOpenModal} />
+        {isModalOpen && selectedMovieId && (
+          <div className="fixed inset-0 bg-black/75 flex justify-center items-center p-4">
+            <div className="bg-white rounded-lg shadow-lg max-w-2xl w-full overflow-hidden">
+              <MovieData imdbID={selectedMovieId} onClose={handleCloseModal} />
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </QueryClientProvider>
     </>
   );
 }
